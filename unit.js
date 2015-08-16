85f25dc357c23c4615555073e85bd04c4e5023a5
Jake White <squirrelboy1225@gmail.com>
2015-08-16 01:24:45 -0400

    fixed upgrades
    added configurable upgrades, fixed the table, added "Surveys", etc

--- unit.js
+++ unit.js
@@ -1,5 +1,9 @@
 var unit = {
-	name: ['Manual Labor', 'Script Kiddy', 'Chain Email'],
-	cost: [10, 25, 50],
-	output: [1, 5, 10]
+	name: ['Manual Labor', 'Script Kiddy', 'Chain Email', 'Botnet'],
+	cost: [10, 100, 1000, 10000],
+	output: [1, 5, 10, 100],
+	description: ['Hire people to manually send out spam messages.',
+	'Get some kids to write out scripts for spam bots.',
+	'Employ the general public--create chain emails!',
+	'Infect networks of computers to do your bidding for you!']
 }
--- upgrade.js
+++ upgrade.js
@@ -1,6 +1,8 @@
 var upgrade = {
-	cost: [1, 5, 10],
+	name: ['Social Media Hack', 'Enlargement Pills', 'Audience Targeting', 'Surveys'],
+	cost: [1, 5, 10, 50],
 	description: ["You hack into Facenovel, a popular social media site, gaining access to personal information.  This doubles spam/second from Manual Labor.",
 "You start sending those kinds of emails.  This increases gullibility by 50%.",
-"You gain information on audiences, and are able to send more appropriate emails based on interests.  This increases gullibility by 50%."]
+"You gain information on audiences, and are able to send more appropriate emails based on interests.  This increases gullibility by 50%.",
+"You get people to answer surveys and provide you with valuable personal information.  This doubles spam/second from chain emails."]
 }
