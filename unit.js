96b99c120ef1847aa361888b4a10010aa2bf8cdc
Jake White <squirrelboy1225@gmail.com>
2015-08-15 22:09:32 -0400

    config files
    added unit and upgrade config js files

---
+++ unit.js
@@ -0,0 +1,5 @@
+var unit = {
+	name: ['Manual Labor', 'Script Kiddy', 'Chain Email'],
+	cost: [10, 25, 50],
+	output: [1, 5, 10]
+}
---
+++ upgrade.js
@@ -0,0 +1,6 @@
+var upgrade = {
+	cost: [1, 5, 10],
+	description: ["You hack into Facenovel, a popular social media site, gaining access to personal information.  This doubles spam/second from Manual Labor.",
+"You start sending those kinds of emails.  This increases gullibility by 50%.",
+"You gain information on audiences, and are able to send more appropriate emails based on interests.  This increases gullibility by 50%."]
+}
